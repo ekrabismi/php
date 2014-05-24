4ba3ab8a6de3d794eda4d24f6a91d42946a52329
Ekra Bismi <ekrabismi@gmail.com>
2014-05-24 17:56:53 +0600

    get country, city

--- php/getCountryInfo/best-1.php
+++ php/getCountryInfo/best-1.php
@@ -1,7 +1,24 @@
 <?php
-//$IP  = "181.197.75.17";
-$IP  = "58.97.141.19";
-$url ="http://api.netimpact.com/qv1.php?key=79facd6luuU2mXLP&qt=geoip&d=json&q=$IP";
+$ip = gethostbyname(isset($REMOTE_ADDR));
+ 
+ if(!empty($ip))
+	 {
+	  
+	 }
+	elseif (!empty($_SERVER['HTTP_CLIENT_IP']))
+	{
+     $ip = $_SERVER['HTTP_CLIENT_IP'];
+	}
+	elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) 
+	{
+		$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	} 
+	else 
+	{
+		$ip = $_SERVER['REMOTE_ADDR'];
+	}
+
+$url ="http://api.netimpact.com/qv1.php?key=79facd6luuU2mXLP&qt=geoip&d=json&q=$ip";
 $info = json_decode(file_get_contents($url));
 echo "<pre>";
 print_r($info);
